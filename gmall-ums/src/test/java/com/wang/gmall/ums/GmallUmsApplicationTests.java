5794dd9117aa01048ee228e91d2d088ac56cbbd8
wang <2596564760@qq.com>
2020-02-04 18:15:03 +0800

    gmall商城项目框架

---
+++ gmall-ums/src/test/java/com/wang/gmall/ums/GmallUmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wang.gmall.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallUmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
